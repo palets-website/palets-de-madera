fb1c21a91146e1964bfe0dfcad688031f44b4fab
Zakamsky <alex.zakamsky@gmail.com>
2021-02-06 22:03:27 +0100

    sorces moved in _src

--- .eleventy.js
+++ .eleventy.js
@@ -121,7 +121,7 @@ module.exports = function(eleventyConfig) {
 
     // These are all optional, defaults are shown:
     dir: {
-      input: ".",
+      input: "_src",
       includes: "_includes",
       data: "_data",
       output: "_site"
